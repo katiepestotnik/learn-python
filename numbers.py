8e53816a72bf59e7497946ee8f685f232b7ab4bf
Katie Pestotnik <katie.pestotnik@gmail.com>
2022-02-18 10:53:51 -0700

    math

---
+++ numbers.py
@@ -0,0 +1,50 @@
+#immutable
+num = 10
+#binary format
+binary = 0b10
+#10 in binary = 2
+print(binary)
+# bin returns binary representation
+print(bin(100))
+#hexadecimal numbers
+hexa = 0xf
+#15
+print(hexa)
+# print in hex format
+print(hex(hexa))
+#complex numbers only used for math not application development
+#j represents imaginary nubmer
+x = 1 + 2j
+print(x)
+
+# math operators
+
+y = 10 + 3
+y = 10 - 3
+y = 10 * 3
+y = 10**3
+print(y)
+#float number
+y = 10 / 3
+print(y)
+#int number with //
+inte = 10//3
+print(inte)
+#remainder of division
+modulus = 10%3
+print(modulus)
+x = 10
+x = x + 1
+print(x)
+x += 1
+
+#cannot use x++ x--
+# use for math functions
+import math
+
+#caps indicate const
+PI = 3.14
+print(round(PI))
+#absolute
+print(abs(PI))
+print(math.floor(PI))
