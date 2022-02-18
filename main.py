c7b99a76dcb34e636aa631651cf9bf4126ed2a8f
Katie Pestotnik <katie.pestotnik@gmail.com>
2022-02-18 09:58:14 -0700

    first commit

---
+++ main.py
@@ -0,0 +1,21 @@
+students_count: int = 100
+students_count = "hundred"
+print(type(students_count))
+rating = 4.99
+is_published = False
+course_name = "Python"
+fun = 'test'
+
+x, y = 2, 3
+a = b = 1
+print(a)
+print(y)
+# integers are immutable x = 2
+# lists are mutable address stays the same
+x = [1, 2, 3]
+# shows memory address
+print(id(x))
+x.append(4)
+print(id(x))
+
+
